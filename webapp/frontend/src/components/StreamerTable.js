94daa710af0b32f8ac0a3258aecf0eb4d23d03d2
Daniel Gao <ganieldao@gmail.com>
2021-08-21 23:12:51 -0400

    Display data on frontend (MVP)

--- webapp/frontend/src/components/StreamerTable.js
+++ webapp/frontend/src/components/StreamerTable.js
@@ -1,26 +1,16 @@
 import { Component } from 'react'
 import './StreamerTable.css'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import { DataGrid } from '@material-ui/data-grid';
 import arr from '../assets/images/arr.png';
 import heavensward from '../assets/images/heavensward.png';
+
+const SAMPLE_DATA = {"status":"success","data":[{"_id":"6119b80dab9597e7fc4a8764","user_login":"richwcampbell","last_updated":"2021-08-22T00:26:41.275Z","quest":{"index":562,"quest":"Storm on the Horizon","quest_link":"https://ffxiv.consolegameswiki.com/wiki/Storm_on_the_Horizon","level":70,"quest_giver":"Hien","giver_link":"https://ffxiv.consolegameswiki.com/wiki/Hien","unlocks":[],"subsection":"","section":"Patch 4.2 (Rise of a New Sun)"}},{"_id":"6119b80dab9597e7fc4a8765","user_login":"fextralife","quest_name":"","last_updated":"2021-08-18T02:56:31.305Z","quest":{"index":156,"quest":"Representing the Representative","quest_link":"https://ffxiv.consolegameswiki.com/wiki/Representing_the_Representative","level":42,"quest_giver":"Ceana","giver_link":"https://ffxiv.consolegameswiki.com/wiki/Ceana","unlocks":[],"subsection":"","section":"Levels 41-50"}},{"_id":"6119b80dab9597e7fc4a8766","user_login":"sloot","last_updated":"2021-08-22T00:16:09.284Z","quest":{"index":336,"quest":"New Winds, Old Friends","quest_link":"https://ffxiv.consolegameswiki.com/wiki/New_Winds,_Old_Friends","level":54,"quest_giver":"Estinien","giver_link":"https://ffxiv.consolegameswiki.com/wiki/Estinien","unlocks":[],"subsection":"","section":"Levels 54-55"}},{"_id":"6119b80dab9597e7fc4a8768","user_login":"shiphtur","last_updated":"2021-08-17T00:00:38.828Z","quest":{"index":587,"quest":"The Syrcus Trench","quest_link":"https://ffxiv.consolegameswiki.com/wiki/The_Syrcus_Trench","level":70,"quest_giver":"Tataru","giver_link":"https://ffxiv.consolegameswiki.com/wiki/Tataru","unlocks":[],"subsection":"Main Quest Chain","section":"Levels 70-71"}}]}
 
 class StreamerTable extends Component {
     constructor() {
         super();
-        const rows = [
-            this.createData('Asmongold', 'Patch 3.1 (As Goes Light, So Goes Darkness)', 'As Goes Light, So Goes Darkness', '08-08-2021', heavensward),
-            this.createData('Asmongold', 'Heavensward Main Scenario', 'asdf', 'asdf', arr),
-            this.createData('Asmongold', 'Heavensward Main Scenario', 'asdf', 'asdf', arr),
-            this.createData('Asmongold', 'Heavensward Main Scenario', 'asdf', 'asdf', arr)
-        ];
         this.state = {
-            rows: rows
+            rows: []
         };
     }
 
@@ -28,40 +18,65 @@ class StreamerTable extends Component {
         fetch("/api/streamers")
             .then(response => response.json())
             .then(json => {
-                console.log(json);
+                this.updateRows(json.data)
             });
     }
 
-    createData(streamer, questline, quest, lastChecked, backgroundImage) {
-        return { streamer, questline, quest, lastChecked, backgroundImage };
+    updateRows(streamers) {
+        var rows = [];
+        var id = 1;
+        streamers.forEach(streamer => {
+            rows.push(this.createData(id++, streamer["user_login"], streamer["quest"]["quest"], 
+                streamer["quest"]["section"], new Date(streamer["last_updated"]), ""));
+        });
+        this.setState({
+            rows: rows
+        });
+        return rows
+    }
+
+    createData(id, userLogin, quest, section, lastUpdated, backgroundImage) {
+        return { id: id, userLogin: userLogin, quest: quest, section: section, lastUpdated };
+    }
+
+    getColumns() {
+        return [
+            {
+                field: 'userLogin',
+                headerName: 'Streamer',
+                width: '200'
+            },
+            {
+                field: 'quest',
+                headerName: 'Quest',
+                width: '500'
+            },
+            {
+                field: 'section',
+                headerName: 'Section',
+                width: '700'
+            },
+            {
+                field: 'lastUpdated',
+                headerName: 'Last Updated',
+                width: '300',
+                valueGetter: this.getLocalDateString
+            }
+        ]
+    }
+
+    getLocalDateString(params) {
+        return params.value.toLocaleDateString() + ' ' + params.value.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
     }
 
     render() {
         return (
-            <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                    <TableCell>Streamer</TableCell>
-                    <TableCell>Quest</TableCell>
-                    <TableCell>Quest-line</TableCell>
-                    <TableCell>Last Checked</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {this.state.rows.map((row) => (
-                    <TableRow key={row.name}>
-                        <TableCell component="th" scope="row">
-                        {row.streamer}
-                        </TableCell>
-                        <TableCell>{row.quest}</TableCell>
-                        <TableCell><img src={row.backgroundImage} style={{width: '100px', verticalAlign: 'middle'}} />{row.questline}</TableCell>
-                        <TableCell>{row.lastChecked}</TableCell>
-                    </TableRow>
-                    ))}
-                </TableBody>
-                </Table>
-            </TableContainer>
+            <div style={{ height: '100%', width: '100%' }}>
+                <DataGrid
+                    rows={this.state.rows}
+                    columns={this.getColumns()}
+                />
+            </div>
         );
     }
 }
