9212402793d4598b405c9a789c68d28b3531a9ca
Daniel Gao <ganieldao@gmail.com>
2021-08-21 20:25:42 -0400

    Webapp fetch from mongo

--- webapp/server.js
+++ webapp/server.js
@@ -2,12 +2,26 @@ const express = require("express");
 const path = require('path');
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch(e => {
     console.error('Mongo connection error', e.message)
   })
-  
-const db = mongoose.connection
+
+var streamerSchema = new mongoose.Schema({
+  "user_login": {
+    type: String
+  },
+  "quest": {
+    type: Object
+  },
+  "last_updated": {
+    type: Date
+  }
+});
+
+var streamerModel = mongoose.model('Streamers', streamerSchema);
+
+
 
 const app = express();
 
@@ -15,8 +29,13 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-app.get("/api/streamers", (req, res) => {
-  res.json({test: "test"});
+app.get("/api/streamers", async (req, res) => {
+  try {
+    const streamers = await streamerModel.find({"quest" : { "$nin": [ null, "" ] }}).exec();
+    res.status(200).json({status: 'success', data: streamers});
+  } catch (err) {
+    res.status(404).json({status: 'fail', message: err});
+  }
 });
 
 app.get('*', (req, res) => {
