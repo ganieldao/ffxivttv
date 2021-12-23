c795901c87cc93411ad13732fe7618b564b582d6
ganieldao <ganieldao@gmail.com>
2021-12-23 12:46:12 -0500

    Prettify streamers

--- webapp/frontend2/pages/index.js
+++ webapp/frontend2/pages/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 import { FixedSizeList as List } from "react-window";
 
 const QUESTS = require('../res/quests_list.json');
-const TEST_DATA = [
-  {
-    "user_login": "scarra",
-    "quest" : {
-      "quest_index": 5
-    }
-  },
-  {
-    "user_login": "cohhcarnage",
-    "quest": {
-      "quest_index": 30
-    }
-  }
-]
+const TEST_DATA = require('../res/test_data.json')['data'];
 
 const questListRef = React.createRef();
 
@@ -30,10 +17,13 @@ export async function getStaticProps(context) {
 }
 
 const StreamerRow = ({ index, style }) => (
-  <div style={style} className="p-4 mb-3 flex justify-between items-center 
-  bg-white shadow rounded-lg hover:bg-sky-700"
-    onClick={() => questListRef.current.scrollToItem(TEST_DATA[index]["quest"]["quest_index"])}>
-    {TEST_DATA[index]["user_login"]}
+  <div style={style} className="flex justify-between items-center">
+    <div className="p-2 flex h-3/4 items-center w-full bg-white shadow rounded-lg hover:bg-sky-700"
+      onClick={() => questListRef.current.scrollToItem(TEST_DATA[index]["quest"]["index"])}>
+      <div>
+        {TEST_DATA[index]["user_login"]}
+      </div>
+    </div>
   </div>
 );
 
@@ -47,16 +37,16 @@ function Home({ streamers }) {
   return (
     <div className="min-h-screen w-screen bg-gray-200 flex flex-col pt-20 justify-center items-center md:items-start md:flex-row">
       <List
-        height={150}
+        height={500}
         itemCount={TEST_DATA.length}
-        itemSize={35}
+        itemSize={80}
         width={300}
       >
         {StreamerRow}
       </List>
       <List
         ref={questListRef}
-        height={150}
+        height={500}
         itemCount={QUESTS.length}
         itemSize={35}
         width={300}
@@ -67,4 +57,4 @@ function Home({ streamers }) {
   )
 }
 
-export default Home
+export default Home;
