53075b00c1769c02a9248289582b70d6e3104318
ganieldao <ganieldao@gmail.com>
2021-12-23 16:56:38 -0500

    More styling

--- webapp/frontend2/pages/index.js
+++ webapp/frontend2/pages/index.js
@@ -15,15 +15,12 @@ export async function getStaticProps(context) {
 }
 
 const StreamerRow = ({ index, style }) => (
-  <div style={style} className="flex justify-between items-center">
-    <div className="flex items-center
-    p-2 h-3/4 w-full 
-    bg-white shadow rounded-lg 
-    hover:bg-sky-700"
+  <div style={style} className="flex justify-between items-center p-2 bg-gray-50">
+    <div className="flex items-center p-2 h-full w-full bg-white shadow rounded-lg select-none hover:bg-sky-100"
       onClick={() => questListRef.current.scrollToItem(TEST_DATA[index]["quest"]["index"])}>
       <img class="object-cover w-8 h-8 rounded-full outline outline-4 outline-green-600" 
       src={TEST_DATA[index]["profile_image_url"]} alt="Profile image"/>
-      <div className="select-none ml-2">
+      <div className="ml-2">
         {TEST_DATA[index]["user_login"]}
       </div>
     </div>
@@ -38,17 +35,30 @@ const Row = ({ index, style }) => (
 
 function Home({ streamers }) {
   return (
-    <div className="flex flex-col justify-center items-center 
-    pt-20 min-h-screen w-screen bg-gray-200 
-    md:items-start md:flex-row">
-      <List
-        height={500}
-        itemCount={streamers.length}
-        itemSize={80}
-        width={300}
-      >
-        {StreamerRow}
-      </List>
+    <div className="flex flex-col justify-center items-center pt-20 min-h-screen w-screen bg-white md:items-start md:flex-row">
+      {/* Streamer List */}
+      <div className="flex flex-col px-5 pb-5 pt-3 bg-gray-100 rounded-lg shadow gap-2">
+        <h1 className="text-xl font-semibold">Streamers</h1>
+        { /* Sort selection */}
+        <label>
+          <span>Sort by:</span>
+          <select className="ml-1 rounded-sm shadow">
+            <option value={0}>Updated</option>
+            <option value={1}>Name</option>
+            <option value={2}>Progress</option>
+          </select>
+        </label>
+        <List
+          height={500}
+          itemCount={streamers.length}
+          itemSize={80}
+          width={300}
+            className="rounded-lg shadow"
+        >
+          {StreamerRow}
+        </List>
+      </div>
+      {/* Quest List */}
       <List
         ref={questListRef}
         height={500}
