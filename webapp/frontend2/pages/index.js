9631ec68cbb33aeb636134dc2221ca6c2337fce1
ganieldao <ganieldao@gmail.com>
2021-12-26 16:33:38 -0500

    Remove libraries and switch to basic list, update test data

--- webapp/frontend2/pages/index.js
+++ webapp/frontend2/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FixedSizeList as List } from "react-window";
 
 const QUESTS = require('../res/quests_list.json');
 const TEST_DATA = require('../res/test_data.json')['data'];
@@ -16,22 +15,22 @@ export async function getStaticProps(context) {
   }
 }
 
-function StreamerList({ streamers, setSelectedQuestIndex }) {
-  const StreamerRow = ({ index, style }) => (
-    <div style={style} className="flex justify-between items-center p-2 bg-gray-100">
-      <div className="flex items-center p-2 h-full w-full bg-white shadow rounded-lg select-none hover:bg-sky-100"
-        onClick={() => setSelectedQuestIndex(streamers[index]["quest"]["index"])}>
-        <img className="object-cover w-8 h-8 rounded-full outline outline-4 outline-green-600"
-          src={streamers[index]["profile_image_url"]} alt="Profile image" />
-        <div className="ml-2">
-          {streamers[index]["user_login"]}
-        </div>
+const StreamerRow = ({ streamer, setSelectedQuestIndex }) => (
+  <li className="flex justify-between items-center p-2 bg-gray-100">
+    <div className="flex items-center p-2 h-full w-full bg-white shadow rounded-lg select-none hover:bg-sky-100"
+      onClick={() => setSelectedQuestIndex(streamer["quest"]["index"])}>
+      <img className="object-cover w-8 h-8 rounded-full outline outline-4 outline-green-600"
+        src={streamer["profile_image_url"]} alt="Profile image" />
+      <div className="ml-2">
+        {streamer["user_login"]}
       </div>
     </div>
-  );
+  </li>
+);
 
+function StreamerList({ streamers, setSelectedQuestIndex }) {
   return (
-    <div className="flex flex-col px-5 pb-5 pt-3 gap-2 bg-gray-100 rounded-lg shadow">
+    <div className="flex flex-col h-5/6 px-5 pb-5 pt-3 gap-2 bg-gray-100 rounded-lg shadow">
       <h1 className="text-xl font-semibold">Streamers</h1>
       { /* Sort selection */}
       <label>
@@ -42,20 +41,15 @@ function StreamerList({ streamers, setSelectedQuestIndex }) {
           <option value={2}>Progress</option>
         </select>
       </label>
-      <List
-        height={500}
-        itemCount={streamers.length}
-        itemSize={80}
-        width={250}
-      >
-        {StreamerRow}
-      </List>
+      <ul className="overflow-y-scroll">
+        {streamers.map((x, i) => <StreamerRow key={"streamerRow" + i} streamer={x} setSelectedQuestIndex={setSelectedQuestIndex} />) }
+      </ul>
     </div>
   );
 }
 
 const QuestRow = ({ quest, rowRef, selectedQuestIndex }) => (
-  <div ref={rowRef} className={"flex select-none items-center " + (selectedQuestIndex == quest["index"] ? "bg-blue-100" : "")}>
+  <li ref={rowRef} className={"flex select-none items-center " + (selectedQuestIndex == quest["index"] ? "bg-blue-100" : "")}>
     <div className="flex w-full items-center justify-between ml-2">
       {quest["quest"]}
       <div className="flex">
@@ -71,19 +65,21 @@ const QuestRow = ({ quest, rowRef, selectedQuestIndex }) => (
         }
       </div>
     </div>
-  </div>
+  </li>
 );
 
 function QuestTable({ section, rowRefs, selectedQuestIndex }) {
   const section_quests = section["quests"];
 
   return (
-    <div>
-      <div className="sticky top-0 shadow-md text-sm p-1 font-semibold bg-blue-300">{section["section"]}</div>
-      {section_quests.map((x, i) => {
-        return <QuestRow key={"questRow" + x["index"]} quest={x} rowRef={el => rowRefs.current[x["index"]] = el} selectedQuestIndex={selectedQuestIndex} />
-      })}
-    </div>
+    <li className="list-none">
+      <ul>
+        <li className="sticky top-0 shadow-md text-sm p-1 font-semibold bg-blue-300">{section["section"]}</li>
+        {section_quests.map((x, i) => {
+          return <QuestRow key={"questRow" + x["index"]} quest={x} rowRef={el => rowRefs.current[x["index"]] = el} selectedQuestIndex={selectedQuestIndex} />
+        })}
+      </ul>
+    </li>
   );
 }
 
@@ -118,7 +114,7 @@ function Home({ streamers }) {
   const [selectedQuestIndex, setSelectedQuestIndex] = React.useState(-1);
 
   return (
-    <div className="flex flex-col justify-center items-center pt-20 min-h-screen w-screen bg-white md:items-start md:flex-row">
+    <div className="flex flex-col justify-center items-center gap-10 pt-20 h-screen w-screen bg-white md:items-start md:flex-row">
       <StreamerList streamers={streamers} setSelectedQuestIndex={setSelectedQuestIndex} />
       <QuestList quests={QUESTS} selectedQuestIndex={selectedQuestIndex} />
     </div>
