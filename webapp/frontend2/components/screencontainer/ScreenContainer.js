4ab3040eb81e3403bd285532b7740492ea5dc247
Daniel Gao <ganieldao@gmail.com>
2022-02-06 15:59:06 -0500

    Start screenshot

---
+++ webapp/frontend2/components/screencontainer/ScreenContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ScreenContainer() {
+    return (
+        <div className="flex flex-col px-5 pb-5 pt-3 h-96 gap-2 bg-gray-100 rounded-lg shadow">
+            test
+        </div>
+    );
+}
+
+export default ScreenContainer;
--- webapp/frontend2/pages/index.js
+++ webapp/frontend2/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import QuestList from "../components/questlist/QuestList";
 import StreamerList from '../components/streamerlist/StreamerList';
+import ScreenContainer from '../components/screencontainer/ScreenContainer';
 
 const QUESTS = require('../res/quests_list.json');
 const TEST_DATA = require('../res/test_data.json')['data'];
@@ -20,7 +21,10 @@ function Home({ streamers }) {
   return (
     <div className="flex flex-col justify-center items-center gap-10 pt-20 h-screen w-screen bg-white md:items-start md:flex-row">
       <StreamerList streamers={streamers} setSelectedQuestIndex={setSelectedQuestIndex} />
-      <QuestList quests={QUESTS} selectedQuestIndex={selectedQuestIndex} />
+      <div>
+        <QuestList quests={QUESTS} selectedQuestIndex={selectedQuestIndex} />
+        <ScreenContainer />
+      </div>
     </div>
   )
 }
