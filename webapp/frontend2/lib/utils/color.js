8796e9dd91aed0699fe5dfd7e452aa0bf4dbe960
Daniel Gao <ganieldao@gmail.com>
2022-02-06 16:22:38 -0500

    Exclude bg-color from purge

--- webapp/frontend2/lib/utils/color.js
+++ webapp/frontend2/lib/utils/color.js
@@ -3,7 +3,7 @@ import { EXPANSION_START_INDICES } from './constants';
 function getRowColor(rowIndex, rowColors) {
     for (let i = 0; i < EXPANSION_START_INDICES.length; i ++) {
         if (rowIndex >= EXPANSION_START_INDICES[i]) {
-        return rowColors[i];
+            return rowColors[i];
         }
     }
 }
