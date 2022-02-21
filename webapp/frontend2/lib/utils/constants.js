3962a4f4c19b68ae5fc5f64185e3e0ec09d82637
Dan Gao <ganieldao@gmail.com>
2022-02-20 22:29:58 -0500

    Replace frontend2

--- webapp/frontend2/lib/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const EXPANSION_START_INDICES = [856, 748, 591, 429, 291, 0]; // TBC, EW, ShB, SB, HW, ARR
-export const QUEST_ROW_COLORS = ["bg-green-200", "bg-gray-200", "bg-purple-200", "bg-red-200", "bg-blue-200", "bg-yellow-100"];
-export const SECTION_ROW_COLORS = ["bg-green-300", "bg-gray-300", "bg-purple-300", "bg-red-300", "bg-blue-300", "bg-yellow-200"];
--- webapp/frontend2/tailwind.config.js
+++ webapp/frontend2/tailwind.config.js
@@ -1,15 +1,7 @@
 module.exports = {
-  content: [    
-    "./pages/**/*.{js,ts,jsx,tsx}",    
-    "./components/**/*.{js,ts,jsx,tsx}",  
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  safelist: [
-    {
-      pattern: /^bg/,
-    },
-  ]
 }
